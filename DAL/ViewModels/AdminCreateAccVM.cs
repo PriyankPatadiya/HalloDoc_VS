9fd048dabe457d3373ca2e17b819e234c77d0f78
PriyankPatadiya <priyank.patadiya@etatvasoft.com>
2024-04-15 19:36:43 +0530

    mm

--- DAL/ViewModels/AdminCreateAccVM.cs
+++ DAL/ViewModels/AdminCreateAccVM.cs
@@ -1,7 +1,9 @@
 ﻿using DAL.DataModels;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,12 +35,15 @@ namespace DAL.ViewModels
 
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid email address")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@(gmail\.com|yahoo\.com|gov\.in)$", ErrorMessage = "Enter a valid email address with valid domain")]
         public string? Email { get; set; }
 
         [Compare("Email", ErrorMessage = "Email and confirm email must match")]
         public string? ConfirmEmail { get; set; }
 
-        [RegularExpression(@"^\d{10}$", ErrorMessage = "Mobile number must be 10 digits")]
+        [Column(TypeName = "character varying")]
+        [Required(ErrorMessage = " Phone number Is Required")]
+        [RegularExpression(@"^(?:(\+?91|0)?[ ]?([\-\s]?[6-9]\d{9})|(\+?91|0)?[ ]?(\d{5})[ ]?(\d{5}))$", ErrorMessage = "Enter a valid Phone number")]
         public string? MobileNo { get; set; }
 
         public string? Address1 { get; set; }
@@ -51,6 +56,7 @@ namespace DAL.ViewModels
 
         public int? State { get; set; }
 
+        [Required(ErrorMessage = "ZipCode is required!")]
         [RegularExpression(@"^\d{5}$", ErrorMessage = "Zip code must be 5 digits")]
         public string? ZipCode { get; set; }
 
